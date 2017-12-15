3d81cc49acb00157702a1c1acc8269e0dc0e76d3
Adam <adam.lin@loginradius.com>
2017-12-15 09:32:13 -0800

    Frameworks add ons - Bootstrap, Animated.css

--- js/script.js
+++ js/script.js
@@ -1,44 +1,46 @@
-// $(document).ready(function() {
+$(document).ready(function() {
 
-// 	// Smooth Scroll Transition Effect
+	// ==========================================================================
+	// Nagvigation  Hide/Show base from Scroll
+	// ==========================================================================
+	var didScroll;
+	var lastScrollTop = 0;
+	var delta = 5;
+	var navbarHeight = $('nav.navbar').outerHeight();
 
-// 	$('a[href*="#"]')
-//     // Remove links that don't actually link to anything
-//     .not('[href="#"]')
-//     .not('[href="#0"]')
-//     .click(function(event) {
-//         // On-page links
-//         if (
-//             location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
-//             location.hostname == this.hostname
-//         ) {
-//             // Figure out element to scroll to
-//             var target = $(this.hash);
-//             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-//             // Does a scroll target exist?
-//             if (target.length) {
-//                 // Only prevent default if animation is actually gonna happen
-//                 event.preventDefault();
-//                 $('html, body').animate({
-//                     scrollTop: target.offset().top
-//                 }, 1000, function() {
-//                     // Callback after animation
-//                     // Must change focus!
-//                     var $target = $(target);
-//                     // $target.focus();
-//                     if ($target.is(":focus")) { // Checking if the target was focused
-//                         return false;
-//                     } else {
-//                         $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
-//                         // $target.focus(); // Set focus again
-//                     };
-//                 });
-//             }
-//         }
-//     });
+	$(window).scroll(function(event){
+		didScroll = true;
+	});
 
-//     // Smooth Scroll Transition Effect end
-// });
+	setInterval(function() {
+		if (didScroll) {
+			hasScrolled();
+			didScroll = false;
+		}
+	}, 250);
+
+	function hasScrolled() {
+		var st = $(this).scrollTop();
+		
+		// Make sure they scroll more than delta
+		if(Math.abs(lastScrollTop - st) <= delta)
+			return;
+
+		// If they scrolled down and are past the navbar, add class .nav-up.
+		// This is necessary so you never see what is "behind" the navbar.
+		if (st > lastScrollTop && st > navbarHeight){
+			// Scroll Down
+			$('nav.navbar').removeClass('nav-down').addClass('nav-up');
+		} else {
+			 // Scroll Up
+			if(st + $(window).height() < $(document).height()) {
+				$('nav.navbar').removeClass('nav-up').addClass('nav-down');
+			}
+		}
+		lastScrollTop = st;
+	}
+
+});
 
 
 
